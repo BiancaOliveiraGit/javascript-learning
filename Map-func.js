26aecfabfaef669f87bd96226ecebea6768879f4
Bianca Oliveira <bianca@abngroup.com.au>
2020-05-04 07:31:04 +1000

    map returning function is cool

--- Map-func.js
+++ Map-func.js
@@ -3,11 +3,20 @@ let somefun = function(){
   console.log('some func called');
   return 5;
 };
+let otherfun = function(){
+  console.log('another func called');
+  return 10;
+};
 
 var mapfunc = new Map();
-mapfunc.set(1, somefun);
+mapfunc.set('x', somefun);
+mapfunc.set('y', otherfun);
 
-let val =mapfunc.get(1)(); //the extra () will call the function straight away
-
+let val = mapfunc.get('x')(); //the extra () will call the function straight away
 console.log(val);
 
+let findval = 'y';
+let result = mapfunc.get(findval); // with no () then it returns a function
+console.log(result());
+
+console.log(5 * mapfunc.get(findval)());
